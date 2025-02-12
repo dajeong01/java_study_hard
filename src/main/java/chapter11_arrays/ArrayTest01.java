6fa7779b7a4dff66f25ecfbe5ee82a1420419a64
dajeong01 <wpekwjd@naver.com>
2025-02-12 17:45:02 +0900

    feat:20250213

---
+++ src/main/java/chapter11_arrays/ArrayTest01.java
@@ -0,0 +1,12 @@
+package chapter11_arrays;
+
+/*
+    배열의 필요성 :
+        여태까지의 수업을 기준으로 하면 10개의 정수형 데이터를 저장하려면
+
+ */
+public class ArrayTest01 {
+    public static void main(String[] args) {
+
+    }
+}
